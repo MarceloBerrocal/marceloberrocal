7d38218812e6f25920f4cb8a32e6b0e9afc8b3eb
Marcelo Zair <marceloberrocal2004@gmail.com>
2021-02-14 10:59:50 -0500

    Cambiar descripcion en el serction

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -9,7 +9,7 @@ const Section = () => {
                 <div className="textos">
                     <div className="title">Marcelo Berrocal Chuque</div>
                     <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni corrupti perspiciatis nostrum saepe odio quidem. Magnam voluptas dolorum fugit quae, sapiente deserunt aut omnis, voluptates cupiditate error non eos dolores, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, possimus sed beatae autem sequi accusantium perferendis reiciendis odit quam fuga labore, nisi debitis totam dolores neque inventore? Suscipit, laudantium et.
+                        Hola, me llamo Marcelo Zair y soy un joven con una gran pasicion por la tecnología.
                     </p>
                 </div>
                 <div className="imgs">
